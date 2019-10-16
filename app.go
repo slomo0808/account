17fb84043271b41f17b786ff266cd4f946f0476c
slomo0808 <94443118@qq.com>
2019-10-17 04:13:26 +0800

    包名重构

--- app.go
+++ app.go
@@ -12,7 +12,6 @@ func init() {
 	infra.Register(&base.PropsStarter{})
 	infra.Register(&base.DbxDatabaseStarter{})
 	infra.Register(&base.ValidatorStarter{})
-	infra.Register(&base.GoRPCStarter{})
 	infra.Register(&base.IrisServerStarter{})
 	infra.Register(&infra.WebApiStarter{})
 	infra.Register(&base.EurekaStarter{})
